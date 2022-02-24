74f12edd9aaa201d7c7610245e682d8c2b13a2ed
Dimpal Patil <dimpalpatil6498@gmail.com>
2022-02-24 11:10:26 +0530

    admin file

--- online-clothing-backend-nodejs-app/src/order.js
+++ online-clothing-backend-nodejs-app/src/order.js
@@ -2,7 +2,7 @@ const Pool = require('pg').Pool;
 const pool = new Pool({
     user: 'postgres',
     host: 'localhost',
-    database: 'online-clothing',
+    database: 'onlineclothing',
     password: 'root',
     port: 5432,
   });
