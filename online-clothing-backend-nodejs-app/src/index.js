33ba86e9b9a7da4cde8e57f4981250907890a240
Neha Prabhu <nehagprabhu1@gmail.com>
2022-02-24 09:12:58 +0530

    Nodejs files

--- online-clothing-backend-nodejs-app/src/index.js
+++ online-clothing-backend-nodejs-app/src/index.js
@@ -1,10 +1,20 @@
 const express = require('express');
 //console.log(express);
 //app will create a web server
+const reqi = require('express/lib/request');
+const order = require("./order");
+
 const app = express();
+app.use(express.json());
+
+//Routing
 app.get('/',function(req,res){
     res.send('<h1>Hello World, this is nodejs</h1>');
 });
+app.get('/orders',order.getOrders);
+
+app.get('/orders/:id',order.getOrderByID);
+
 const port = 3000;
 app.listen(port,function(){
     console.log("Web server started successfully");
---
+++ online-clothing-backend-nodejs-app/src/order.js
@@ -0,0 +1,31 @@
+const Pool = require('pg').Pool;
+const pool = new Pool({
+    user: 'postgres',
+    host: 'localhost',
+    database: 'online-clothing',
+    password: 'root',
+    port: 5432,
+  });
+
+  function getOrders(req, res) {
+    pool.query("SELECT * FROM orders",(error, result) => {
+      //console.log(result.rows);
+      if (error) {
+        return res.status(500).send("Internal Error on server");
+      } else {
+        return res.status(200).send(result.rows);
+      }
+    });
+  }
+
+  function getOrderByID(req,res){
+    const id = parseInt(req.params.id);
+    pool.query('SELECT * FROM orders WHERE orderid = $1', [id], (error, result) => {
+      if (error) {
+        return res.status(500).send("Internal Error on server");
+      }
+      return res.status(200).json(result.rows);
+    })
+  }
+
+  module.exports = {getOrders,getOrderByID};
