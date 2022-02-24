923334c09ac928c72be087ec358b98b0ac6aa46b
Neha Prabhu <nehagprabhu1@gmail.com>
2022-02-24 13:13:15 +0530

    Updated files

--- online-clothing-backend-nodejs-app/src/index.js
+++ online-clothing-backend-nodejs-app/src/index.js
@@ -9,11 +9,48 @@ app.use(express.json());
 
 //Routing
 app.get('/',function(req,res){
-    res.send('<h1>Hello World, this is nodejs</h1>');
+    res.send(`<h1>For Orders:</h1>
+    <p><a href="http://localhost:3000/orders">/orders</a></p>
+    <p><a href="http://localhost:3000/orders/1">/orders/1</a></p>
+    <p><a href="http://localhost:3000/orderlines">/orderlines</a></p>
+    <p><a href="http://localhost:3000/orderlines/1">/orderlines/1</a></p>
+    <p><a href="http://localhost:3000/totalamount/1">/totalamount/1</a><p>`);
 });
+/************
+        For Order processing
+******************* */
+
+//get all orders
 app.get('/orders',order.getOrders);
 
+//get particular order
 app.get('/orders/:id',order.getOrderByID);
+
+//get all orderlines - every order has many orderlines
+app.get('/orderlines',order.getOrderLines);
+
+//get all orderlines of particular order, one orderline for each product ordered
+app.get('/orderlines/:id',order.getOrderLinesByOrderId);
+
+//get total amount of one order
+app.get('/totalamount/:id',order.getTotalAmountByOrderId);
+
+//post order
+app.post('/orders',order.insertOrder);
+
+//post orderlines for particular orderid
+app.post('/orderlines/:id',order.insertOrderline);
+
+//put total amount in the order
+app.put('/totalamount/:id',order.updateOrderAmount);
+
+//update orderline
+app.put('/orderlines/:id',order.updateOrderLine);
+
+//delete orderline
+app.delete('/orderlines/:id',order.deleteOrderLine);
+
+//******************************************************************* */
 
 const port = 3000;
 app.listen(port,function(){
--- online-clothing-backend-nodejs-app/src/order.js
+++ online-clothing-backend-nodejs-app/src/order.js
@@ -2,7 +2,7 @@ const Pool = require('pg').Pool;
 const pool = new Pool({
     user: 'postgres',
     host: 'localhost',
-    database: 'online-clothing',
+    database: 'onlineclothing',
     password: 'root',
     port: 5432,
   });
@@ -28,4 +28,55 @@ const pool = new Pool({
     })
   }
 
-  module.exports = {getOrders,getOrderByID};
+  function getOrderLines(req,res){
+      pool.query("SELECT * from orderline",(error,result)=>{
+          if(error){
+              return res.status(500).send("Internal Error on server");
+          }
+          else{
+           
+            return res.json(result.rows);
+        
+          }
+      })
+  }
+
+  function getOrderLinesByOrderId(req,res){
+    const id = parseInt(req.params.id);
+      pool.query("SELECT * from orderline WHERE orderid = $1",[id],(error,result)=>{
+      if(error){
+        return res.status(500).send("Internal Error on server");
+      }
+      return res.json(result.rows);
+    })
+  }
+
+  function getTotalAmountByOrderId(req,res){
+    const id = parseInt(req.params.id);
+    pool.query('select sum(price),orderid from orderline group by orderid having orderid = $1;', [id],(error,result)=>{
+        if(error){
+            return res.status(500).send("Internal Error on server");
+        }
+        else{
+            return res.status(200).json(result.rows);
+        
+        }
+    })
+  }
+  function insertOrder(){
+
+  }
+  function insertOrderline(){
+
+  }
+  function updateOrderAmount(){
+
+  }
+  function deleteOrderLine(){
+
+  }
+  function updateOrderLine(){
+
+  }
+  module.exports = {getOrders,getOrderByID,getOrderLines,getOrderLinesByOrderId,getTotalAmountByOrderId,
+    insertOrder,insertOrderline,updateOrderAmount,updateOrderLine,deleteOrderLine};
